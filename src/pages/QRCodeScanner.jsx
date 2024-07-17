64fe5b0fb7ae17e3902d40a80dff077ac06ad1a8
Rizo <zamonshoev2162@gmail.com>
2024-07-17 21:10:37 +0600

    QRCodeScanner page styling

--- src/pages/QRCodeScanner.jsx
+++ src/pages/QRCodeScanner.jsx
@@ -36,36 +36,65 @@ export const QRCodeScanner = () => {
       setImagePreview(URL.createObjectURL(file));
       QrScanner.scanImage(file)
         .then((result) => setScanned(result))
-        .catch(() => setError('QR code not found'));
+        .catch(() => setError('QR code not found!'));
     }
   }
 
   return (
     <div>
       <Navigation />
-      <div>
-        <div>
-          <Scanner
-            components={{ audio: false }}
-            styles={{ container: { width: 256 } }}
-            onScan={onScanHandler}
-          />
+      <main className="container m-auto flex flex-col gap-4 p-5">
+        <div className="flex justify-between gap-4">
+          <div className="flex flex-col gap-12">
+            <label className="button w-fit">
+              <input
+                className="h-0 w-0 opacity-0"
+                type="file"
+                accept="image/*"
+                onChange={uploadFile}
+              />
+              <span>Upload image</span>
+            </label>
+            {imagePreview && (
+              <div className="max-h-80 w-6/12 overflow-hidden rounded-lg border-2 border-orange-100">
+                <img
+                  className="object-cover"
+                  src={imagePreview}
+                  alt="Preview"
+                />
+              </div>
+            )}
+          </div>
+          <div className="h-64 w-64 bg-black">
+            <Scanner
+              components={{ audio: false, finder: false }}
+              styles={{ container: { width: 256 } }}
+              onScan={onScanHandler}
+            />
+          </div>
         </div>
-        <div>
-          <input type="file" accept="image/*" onChange={uploadFile} />
-          {imagePreview && <img src={imagePreview} alt="Preview" />}
-          {error && <p>{error}</p>}
+
+        <div className="m-auto flex w-6/12 flex-col gap-4 pt-5">
+          {error && <p className="text-center text-4xl font-bold">{error}</p>}
+          {scanned && (
+            <div className="flex justify-center gap-4 text-nowrap">
+              <button className="button h-fit w-fit" onClick={copyToClipboard}>
+                {copySuccess ? 'Copied!' : 'Copy'}
+              </button>
+              {scanLink && (
+                <button className="button h-fit w-fit" onClick={openLink}>
+                  Open Link
+                </button>
+              )}
+            </div>
+          )}
+          {scanned !== '' && (
+            <p className="min-w-64 overflow-x-auto text-wrap rounded-lg border-2 border-orange-100 p-5 text-justify font-bold">
+              {scanned}
+            </p>
+          )}
         </div>
-      </div>
-      <p>{scanned}</p>
-      {scanned && (
-        <div>
-          <button onClick={copyToClipboard}>
-            {copySuccess ? 'Copied!' : 'Copy'}
-          </button>
-          {scanLink && <button onClick={openLink}>Open Link</button>}
-        </div>
-      )}
+      </main>
     </div>
   );
 };
