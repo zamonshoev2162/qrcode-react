f152b52f7b9125ce21d94c0959aa950d0a5b584a
Rizo <zamonshoev2162@gmail.com>
2024-07-18 12:36:05 +0600

    Removing library and modifying QRCodeScanner component

--- src/pages/QRCodeScanner.jsx
+++ src/pages/QRCodeScanner.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { Scanner } from '@yudiel/react-qr-scanner';
 import QrScanner from 'qr-scanner';
 import { Navigation } from '../components/Navigation';
 
@@ -9,13 +8,6 @@ export const QRCodeScanner = () => {
   const [imagePreview, setImagePreview] = useState();
   const [copySuccess, setCopySuccess] = useState();
   const scanLink = scanned.match(/^https?:\/\//);
-
-  function onScanHandler(result) {
-    setScanned('');
-    setError('');
-    setImagePreview('');
-    setScanned(result[0]?.rawValue || '');
-  }
 
   function copyToClipboard() {
     navigator.clipboard.writeText(scanned).then(() => {
@@ -44,55 +36,44 @@ export const QRCodeScanner = () => {
     <div>
       <Navigation />
       <main className="container m-auto flex flex-col gap-4 p-5">
-        <div className="flex justify-between gap-4">
-          <div className="flex flex-col gap-12">
-            <label className="button w-fit">
-              <input
-                className="h-0 w-0 opacity-0"
-                type="file"
-                accept="image/*"
-                onChange={uploadFile}
-              />
-              <span>Upload image</span>
-            </label>
-            {imagePreview && (
-              <div className="max-h-80 w-6/12 overflow-hidden rounded-lg border-2 border-orange-100">
-                <img
-                  className="object-cover"
-                  src={imagePreview}
-                  alt="Preview"
-                />
-              </div>
-            )}
-          </div>
-          <div className="h-64 w-64 bg-black">
-            <Scanner
-              components={{ audio: false, finder: false }}
-              styles={{ container: { width: 256 } }}
-              onScan={onScanHandler}
-            />
-          </div>
-        </div>
-
-        <div className="m-auto flex w-6/12 flex-col gap-4 pt-5">
-          {error && <p className="text-center text-4xl font-bold">{error}</p>}
-          {scanned && (
-            <div className="flex justify-center gap-4 text-nowrap">
-              <button className="button h-fit w-fit" onClick={copyToClipboard}>
-                {copySuccess ? 'Copied!' : 'Copy'}
-              </button>
-              {scanLink && (
-                <button className="button h-fit w-fit" onClick={openLink}>
-                  Open Link
-                </button>
-              )}
+        <label className="button h-fit w-fit">
+          <input
+            className="h-0 w-0 opacity-0"
+            type="file"
+            accept="image/*"
+            onChange={uploadFile}
+          />
+          <span>Upload image</span>
+        </label>
+        <div className="flex w-full flex-col-reverse gap-4 pt-5 md:flex-row">
+          {imagePreview && (
+            <div className="h-64 md:w-6/12">
+              <img className="object-cover" src={imagePreview} alt="Preview" />
             </div>
           )}
-          {scanned !== '' && (
-            <p className="min-w-64 overflow-x-auto text-wrap rounded-lg border-2 border-orange-100 p-5 text-justify font-bold">
-              {scanned}
-            </p>
-          )}
+          <div className="flex flex-col gap-4 md:w-6/12">
+            {error && <p className="text-center text-4xl font-bold">{error}</p>}
+            {scanned && (
+              <div className="flex justify-center gap-4 text-nowrap">
+                <button
+                  className="button h-fit w-fit"
+                  onClick={copyToClipboard}
+                >
+                  {copySuccess ? 'Copied!' : 'Copy'}
+                </button>
+                {scanLink && (
+                  <button className="button h-fit w-fit" onClick={openLink}>
+                    Open Link
+                  </button>
+                )}
+              </div>
+            )}
+            {scanned !== '' && (
+              <p className="min-w-64 overflow-x-auto text-wrap rounded-lg border-2 border-orange-100 p-5 text-justify">
+                {scanned}
+              </p>
+            )}
+          </div>
         </div>
       </main>
     </div>
